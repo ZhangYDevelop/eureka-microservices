6c1197e19fdfceb30da55825930af6389f7d9e37
ZhangYDevelop <2389311433@qq.com>
2019-12-14 16:54:08 +0800

    依赖调整

--- gateway/src/main/java/com/zy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/zy/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
 package com.zy.gateway;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2Doc
 @EnableZuulProxy
 public class GatewayApplication {
 
