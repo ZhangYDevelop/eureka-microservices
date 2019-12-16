1eef93911016bab7132b8bde0e469bfc1f9dee81
ZhangYDevelop <2389311433@qq.com>
2019-12-16 09:14:42 +0800

    框架调整

--- uaa/src/main/java/com/zy/uaa/mapper/ClientServiceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.zy.uaa.mapper;
-
-import com.zy.uaa.entity.ClientService;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ClientServiceMapper extends Mapper<ClientService> {
-    void deleteByServiceId(int id);
-}
